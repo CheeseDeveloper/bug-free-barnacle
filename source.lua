19edacf986835a906db100f0ed7b535a6ea231ef
crlMinXx <86245870+CheeseDeveloper@users.noreply.github.com>
2022-05-30 18:23:38 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,57 +1,10 @@
-local library = {}
+local player = {}
 
-function library:createGui()
-    local NewLibrary = Instance.new("ScreenGui")
-    local libFrame = Instance.new("Frame")
-    
-    NewLibrary.Name = "NewLibrary"
-    NewLibrary.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-    NewLibrary.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    
-    libFrame.Name = "libFrame"
-    libFrame.Parent = NewLibrary
-    libFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    libFrame.Size = UDim2.new(0.105246596, 0, 0.157902971, 0)
-end
-
-function library:setTitle(text : string)
-    if not libFrame.libTitle then
-        local libTitle = Instance.new("TextLabel")
-        local UICorner = Instance.new("UICorner")
-        
-        libTitle.Name = "libTitle"
-        libTitle.Parent = game.StarterGui.NewLibrary.libFrame
-        libTitle.BackgroundColor3 = Color3.fromRGB(9, 9, 9)
-        libTitle.BorderSizePixel = 0
-        libTitle.Size = UDim2.new(1.00000036, 0, 0.0891264975, 0)
-        libTitle.Font = Enum.Font.FredokaOne
-        libTitle.Text = text
-        libTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
-        libTitle.TextScaled = true
-        libTitle.TextSize = 14.000
-        libTitle.TextWrapped = true
-        UICorner.CornerRadius = UDim.new(0.200000003, 0)
-        UICorner.Parent = libTitle
-    else
-        libFrame.libTitle:Destroy()
-        
-        local libTitle = Instance.new("TextLabel")
-        local UICorner = Instance.new("UICorner")
-        
-        libTitle.Name = "libTitle"
-        libTitle.Parent = game.StarterGui.NewLibrary.libFrame
-        libTitle.BackgroundColor3 = Color3.fromRGB(9, 9, 9)
-        libTitle.BorderSizePixel = 0
-        libTitle.Size = UDim2.new(0, 399, 0, 24)
-        libTitle.Font = Enum.Font.FredokaOne
-        libTitle.Text = text
-        libTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
-        libTitle.TextScaled = true
-        libTitle.TextSize = 14.000
-        libTitle.TextWrapped = true
-        UICorner.CornerRadius = UDim.new(0.200000003, 0)
-        UICorner.Parent = libTitle
+function player:setWalkSpeed(plr : string, value : NumberValue)
+    local player = game.Players[plr]
+    if player and player.Character then
+        player.Character:FindFirstChild("Humanoid").WalkSpeed = value
     end
 end
 
-return library
+return player
