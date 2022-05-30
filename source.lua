eec9e00397b6cea6c18e8e06d532367661573733
crlMinXx <86245870+CheeseDeveloper@users.noreply.github.com>
2022-05-30 17:53:18 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,10 +1,15 @@
 local library = {}
 
 function library:createGui()
+    local NewLibrary = Instance.new("ScreenGui")
     local libFrame = Instance.new("Frame")
-
+    
+    NewLibrary.Name = "NewLibrary"
+    NewLibrary.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    NewLibrary.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    
     libFrame.Name = "libFrame"
-    libFrame.Parent = game.StarterGui.NewLibrary
+    libFrame.Parent = NewLibrary
     libFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     libFrame.Size = UDim2.new(0.105246596, 0, 0.157902971, 0)
 end
