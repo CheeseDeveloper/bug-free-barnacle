9205a62d0d57cabcd77c96757cc96e39ddb8101f
crlMinXx <86245870+CheeseDeveloper@users.noreply.github.com>
2022-05-30 18:14:43 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -14,4 +14,44 @@ function library:createGui()
     libFrame.Size = UDim2.new(0.105246596, 0, 0.157902971, 0)
 end
 
+function library:setTitle(text)
+    if not libFrame.libTitle then
+        local libTitle = Instance.new("TextLabel")
+        local UICorner = Instance.new("UICorner")
+        
+        libTitle.Name = "libTitle"
+        libTitle.Parent = game.StarterGui.NewLibrary.libFrame
+        libTitle.BackgroundColor3 = Color3.fromRGB(9, 9, 9)
+        libTitle.BorderSizePixel = 0
+        libTitle.Size = UDim2.new(0, 399, 0, 24)
+        libTitle.Font = Enum.Font.FredokaOne
+        libTitle.Text = text
+        libTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+        libTitle.TextScaled = true
+        libTitle.TextSize = 14.000
+        libTitle.TextWrapped = true
+        UICorner.CornerRadius = UDim.new(0.200000003, 0)
+        UICorner.Parent = libTitle
+    else
+        libFrame.libTitle:Destroy()
+        
+        local libTitle = Instance.new("TextLabel")
+        local UICorner = Instance.new("UICorner")
+        
+        libTitle.Name = "libTitle"
+        libTitle.Parent = game.StarterGui.NewLibrary.libFrame
+        libTitle.BackgroundColor3 = Color3.fromRGB(9, 9, 9)
+        libTitle.BorderSizePixel = 0
+        libTitle.Size = UDim2.new(0, 399, 0, 24)
+        libTitle.Font = Enum.Font.FredokaOne
+        libTitle.Text = text
+        libTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+        libTitle.TextScaled = true
+        libTitle.TextSize = 14.000
+        libTitle.TextWrapped = true
+        UICorner.CornerRadius = UDim.new(0.200000003, 0)
+        UICorner.Parent = libTitle
+    end
+end
+
 return library
